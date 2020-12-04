2b28262817198cc53bc946a07df80b491dbac50d
Levi Crews <levicrews24@gmail.com>
2020-12-04 08:29:51 -0600

    bootstrap package installation

---
+++ .config/yadm/bootstrap.sh
@@ -0,0 +1,38 @@
+#! /usr/bin/env sh
+
+# References: https://github.com/rkalis/dotfiles and associated blog posts
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+source coloredecho.sh
+
+info "Prompting for sudo password..."
+if sudo -v; then
+    # Keep-alive: update existing `sudo` time stamp until `setup.sh` has finished
+    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+    success "Sudo credentials updated."
+else
+    error "Failed to obtain sudo credentials."
+fi
+
+COMMENT=\#*
+
+sudo -v
+
+find * -name "*.list" | while read fn; do
+    cmd="${fn%.*}" # Bash string operator
+    set -- $cmd
+    info "Installing $1 packages..."
+    while read package; do
+        if [[ $package == $COMMENT ]]; then continue; fi
+        if [[ $cmd == code ]]; then
+            substep_info "Installing VSCode extension $package..."
+            $cmd --install-extension $package
+        else
+            substep_info "Installing $package..."
+            $cmd install $package
+        fi
+    done < "$fn"
+    success "Finished installing $1 packages."
+done
