8df270073e3eb8ec4af1031bd0203afae3438bf8
Levi Crews <levicrews24@gmail.com>
2020-12-07 20:09:20 -0600

    change doom-theme, don't fontify DONE headlines

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -8,17 +8,19 @@
 
 (setq display-line-numbers-type t)
 
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-zenburn)
+;;(setq doom-theme 'doom-palenight)
 
 (after! org
   (setq org-directory "~/Dropbox/org/"
         org-roam-directory "~/Dropbox/org/roam"
         org-hide-leading-stars t
+        org-fontify-done-headline nil
         auto-save-default nil
         make-backup-files nil
         org-log-done t
         org-log-into-drawer t
         org-clock-into-drawer t
         org-todo-keywords
-        '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d!)" "KILL(k!)")
-            (sequence "INSPECT(i)" "UNDERSTAND(u!)" "EVAL(e!)" "|" "READ(r!)" "KILL(k!)"))))
+        '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d)" "KILL(k)")
+            (sequence "INSPECT(i)" "UNDERSTAND(u!)" "EVAL(e!)" "|" "READ(r)" "KILL(k)"))))
