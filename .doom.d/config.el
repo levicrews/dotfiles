d41991cdaf7cf0750d6a730287c3ce58e2cd854e
Levi Crews <levicrews24@gmail.com>
2021-08-27 06:21:32 -0500

    Doom/config: hide emphasis markers + info on org-ref

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -71,7 +71,18 @@ _q_: quit this menu                         _r_: restart emacs
         org-hide-leading-stars t
         org-startup-indented t
         org-startup-folded t
+        org-hide-emphasis-markers t
         org-fontify-done-headline nil))
+
+(use-package! org-appear
+  :hook (org-mode . org-appear-mode)
+  :config
+  (setq org-appear-autoemphasis t
+        org-appear-autosubmarkers t
+        org-appear-autolinks nil)
+  ;; for proper first-time setup, `org-appear--set-elements'
+  ;; needs to be run after other hooks have acted.
+  (run-at-time nil nil #'org-appear--set-elements))
 
 (add-hook 'org-mode-hook (lambda ()
   "Beautify Org Checkbox Symbol"
