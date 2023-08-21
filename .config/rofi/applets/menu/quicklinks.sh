e6924d049d3b800b47be19fa1049df830bf3d5d4
Levi Crews <levicrews24@gmail.com>
2023-08-21 17:00:30 -0500

    minor rofi menu tweaks

--- .config/rofi/applets/menu/quicklinks.sh
+++ .config/rofi/applets/menu/quicklinks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
+## Mail    : adi1090x@calendar.com
 ## Github  : @adi1090x
 ## Twitter : @adi1090x
 
@@ -26,37 +26,30 @@ else
 fi
 
 # Logos: glyphs from https://www.nerdfonts.com/cheat-sheet
-ucmail="" #Hex: f19d
-gmail="" #Hex: f6ef
+mail="" #Hex: f6ef
+calendar="" #Hex: f073
 brainfm="" #Hex: f7ca
 google="" #Hex: f1a0
 twitter="" #Hex: f099
 feedly="" #Hex: e27b
 github="" #Hex: f09b
-# asana="" #Hex: f7b0
 asana="⛬" #file-icons U+26EC (technically Julia)
-website="" #Hex: f2c3
-# website="Ⓗ" #file-icons U+24bBD (Hugo)
+website="" #Hex: f007
+overleaf="" #FontAwesome: U+F1C9
 
 # Variable passed to rofi
-options="$ucmail\n$gmail\n$brainfm\n$google\n$twitter\n$feedly\n$github\n$asana\n$website"
+options="$mail\n$brainfm\n$calendar\n$feedly\n$github\n$twitter\n$asana\n$website\n$overleaf\n$google"
 
 chosen="$(echo -e "$options" | $rofi_command -p "Open In  :  $app" -dmenu -selected-row 0)"
 case $chosen in
-    $ucmail)
+    $mail)
         $app https://mail.google.com/mail/u/1/#starred &
-        ;;
-    $gmail)
-        $app https://mail.google.com/mail/u/0/#starred &
         ;;
     $brainfm)
         $app https://my.brain.fm/ &
         ;;
-    $google)
-        $app https://www.google.com/ &
-        ;;
-    $twitter)
-        $app https://twitter.com/LeviGCrews/likes &
+    $calendar)
+        $app https://calendar.vimcal.com/home &
         ;;
     $feedly)
         $app https://feedly.com/i/saved &
@@ -64,10 +57,19 @@ case $chosen in
     $github)
         $app https://github.com/levicrews &
         ;;
+    $twitter)
+        $app https://twitter.com/LeviGCrews/likes &
+        ;;
     $asana)
         $app https://app.asana.com/0/home/1202371903456271 &
         ;;
     $website)
         $app https://www.levicrews.com/ &
+        ;;
+    $overleaf)
+        $app https://www.overleaf.com/project &
+        ;;
+    $google)
+        $app https://www.google.com/ &
         ;;
 esac
